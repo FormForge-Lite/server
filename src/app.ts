95f1545061172ba5896823555d454aa73682fcce
androkingdom <androkingdom1@gmail.com>
2025-07-23 21:28:25 +0530

    feat: update Prisma schema and migrations, add user model and API endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
 import express, { Express } from "express";
 import cors from "cors";
+import { PrismaClient } from "./generated/prisma";
 
 const app: Express = express();
-app.use(cors)
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true}));
-app.use(express.static('public'));
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.static("public"));
 
+const prisma = new PrismaClient();
+app.get("/user", async (_, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.json({ error });
+  } finally {
+    await prisma.$disconnect();
+  }
+});
 export default app;
